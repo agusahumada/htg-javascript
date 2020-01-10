dd5d015166ba6ebbabd33cb5cc68e0159b89cb1a
agusaahumada <agustinaahumada649@gmail.com>
2020-01-10 17:45:49 -0300

    ejercicio tecnico3

--- 03 Sentando las bases/01 querySelector/scripts/main.js
+++ 03 Sentando las bases/01 querySelector/scripts/main.js
@@ -1,8 +1,16 @@
 const resaltado =document.querySelector('.resaltado');
 resaltado.classList.add('comentario');
 
-const azul=document.querySelectorAll('.hr');
-for (i=o<azul.length; i++){
-    azul.classList.add('.hr');
+const azul=document.querySelectorAll('hr');
+for (i=0;i<azul.length; i++){
+    azul[i].classList.add('azul');
+console.log(azul);
 }
+var caracteres =document.querySelectorAll('.comentario--texto');
+for( i=0;i<caracteres.length;i++){
+    if(caracteres[i].textContent.length > 50){
+        caracteres[i].classList.add('caracteresOcultos');
+    }
+}
+
 console.info('Cuando veas éste mensaje en consola, ya habrás terminado ésta parte del ejercicio. Borra ésta línea cuando ya la hayas visto alguna vez en la consola al acceder a ésta página.')
