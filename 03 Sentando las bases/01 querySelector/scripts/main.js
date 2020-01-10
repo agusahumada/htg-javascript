3e3101a0f4984a34597448b197aa30f3c72f0be2
agusaahumada <agustinaahumada649@gmail.com>
2020-01-10 16:57:17 -0300

    ejercicio tecnico2

--- 03 Sentando las bases/01 querySelector/scripts/main.js
+++ 03 Sentando las bases/01 querySelector/scripts/main.js
@@ -1,1 +1,8 @@
+const resaltado =document.querySelector('.resaltado');
+resaltado.classList.add('comentario');
+
+const azul=document.querySelectorAll('.hr');
+for (i=o<azul.length; i++){
+    azul.classList.add('.hr');
+}
 console.info('Cuando veas éste mensaje en consola, ya habrás terminado ésta parte del ejercicio. Borra ésta línea cuando ya la hayas visto alguna vez en la consola al acceder a ésta página.')
